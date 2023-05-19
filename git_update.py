2b8fcf7b039c80d9f87476cfd7f0ba0cd6792a45
Khushaalan Arjunan <khushaalanarjunan@gmail.com>
2023-05-19 21:13:28 +0800

    update

--- git_update.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def git_update():
-    os.system('git status')
-    os.system('git add -A')
-    os.system('git commit -m "update"')
-    os.system('git push origin main')
-
-git_update()
